39501a505ea026d75e914709e955c8e59a5d2955
Fumie <amoramnis@gmail.com>
2016-01-05 11:54:11 +0900

    トップページフォーム入力

---
+++ admin/confirm.php
@@ -0,0 +1,14 @@
+<?php
+	var_dump($_POST);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link type="text/css" rel="stylesheet" href="style.css">
+<title></title>
+</head>
+<body>
+
+</body>
+</html>
